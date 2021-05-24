1453035c346a67ca0f6bb121ffbaf491488a16fa
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-24 20:46:05 +0530

    Update mongo.py

--- SheebaQueen/mongo.py
+++ SheebaQueen/mongo.py
@@ -3,22 +3,24 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from SheebaQueen import MONGO_DB_URI 
+from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SheebaQueen.conf import get_int_key, get_str_key
 
+from SheebaQueen import log
+from SheebaQueen.config import get_int_key, get_str_key
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_URI = get_str_key("MONGO_URI")
+MONGO_PORT = get_int_key("MONGO_PORT")
+MONGO_DB = get_str_key("MONGO_DB")
 
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+# Init MongoDB
+mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["SheebaQueen"]
+
+engine = AIOEngine(motor, MONGO_DB)
+
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
