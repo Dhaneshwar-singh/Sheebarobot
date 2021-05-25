3eb7293fdbd91cc00401c16731b95e981daa13e1
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-25 08:17:53 +0530

    Update mongo.py

--- SheebaQueen/mongo.py
+++ SheebaQueen/mongo.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from AsunaRobot import MONGO_DB_URI 
+from SheebaQueen import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from SheebaQueen.conf import get_int_key, get_str_key
