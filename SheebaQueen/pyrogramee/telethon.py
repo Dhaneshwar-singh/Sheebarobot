952665ef945db27288cefd7f0795aecdf025ce93
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-26 08:53:28 +0530

    Update telethon.py

--- SheebaQueen/pyrogramee/telethon.py
+++ SheebaQueen/pyrogramee/telethon.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
 
-from SheebaQueen.confi import get_int_key, get_str_key
+from SheebaQueen.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 NAME = TOKEN.split(":")[0]
