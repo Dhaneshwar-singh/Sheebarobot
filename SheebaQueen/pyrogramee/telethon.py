1eebc157ef4d7116d3fd54cd3fd8461703ebb2bb
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-26 08:48:15 +0530

    Update telethon.py

--- SheebaQueen/pyrogramee/telethon.py
+++ SheebaQueen/pyrogramee/telethon.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
 
-from DaisyX.config import get_int_key, get_str_key
+from SheebaQueen.config import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 NAME = TOKEN.split(":")[0]
