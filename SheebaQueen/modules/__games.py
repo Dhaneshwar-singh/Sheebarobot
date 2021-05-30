760ce263ccdb1ee82a9ee5fcf7d02558f486e2a5
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-30 19:27:22 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -3,7 +3,7 @@ from SheebaQueen.events import register
 from telethon import types, Button, events
 
 
-@game.on(events.NewMessage(pattern="^/game ? (.*)"))
+@game.on(events.register(pattern="^/game ? (.*)"))
 async def game (event):
  return await event.reply("hello, this is message")
 
