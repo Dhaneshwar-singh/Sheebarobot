334923c2a014a3ac09def77596791194cc6eb48e
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-30 23:17:25 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -1,5 +1,5 @@
 from SheebaQueen.events import register
-@register(pattern="^/game ? (.*)"))
+@register(pattern="^/game ? (.*)")
  async def game (event): 
       await event.reply("hello, this is message") 
 __mod_name__ = " Games "
