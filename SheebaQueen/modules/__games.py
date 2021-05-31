69589cb0ddb5c5ab151b210edc9209ec81d481e7
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-31 09:04:22 +0530

    Create __games.py

---
+++ SheebaQueen/modules/__games.py
@@ -0,0 +1,11 @@
+from SheebaQueen.events import register
+
+@register(pattern="^/game")
+
+async def game(event): 
+
+      await event.reply("hello, this is message") 
+
+__mod_name__ = " Games "
+
+__help__ = """ COMING SOON..... """
