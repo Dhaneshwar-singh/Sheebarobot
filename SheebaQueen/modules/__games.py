5ed7bc048cad918a5bf4077387a5cc5e94b02f96
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-31 14:50:42 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -19,7 +19,7 @@ async def game(event):
          
          [InlineKeyboardButton(
 
-                        "PLAY Game", url="t.me/vcmusicplayerlightning"
+                        "PLAY Game", url="https://sheebaga.heliohost.us"
 
                     )
 
