4553315b3c0a5d5a960e984b60e4c3bc66dd6539
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-31 14:49:31 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -8,11 +8,13 @@ async def game(event):
 
       await event.reply("/2048 : 2048 No. making game") 
 #2048 game
+f_img="https://telegra.ph/file/dbe8d49976baf292bb372.png"
+
 @register(pattern="^/2048")
 
 async def game(event): 
 
-      await event.reply(
+      await event.reply(f_img,
          reply_markup = InlineKeyboardMarkup(
          
          [InlineKeyboardButton(
