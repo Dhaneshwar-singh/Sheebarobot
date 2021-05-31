13e8f4bcd04b2259366d3ddcea39b07ae43e87c2
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-31 11:09:10 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -1,12 +1,12 @@
 from SheebaQueen.events import register
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @register(pattern="^/game")
 
 async def game(event): 
 
-      await event.reply("/2048") 
+      await event.reply("/2048 : 2048 No. making game") 
 
 __mod_name__ = " Games "
 
