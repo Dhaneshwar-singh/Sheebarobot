b4bfe02262d70f1872dfcf86caf68315e49dd94b
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-31 12:47:54 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -7,6 +7,19 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def game(event): 
 
       await event.reply("/2048 : 2048 No. making game") 
+#2048 game
+@register(pattern="^/2048")
+
+async def game(event): 
+
+      await event.reply("/2048 : 2048 No. making game") 
+
+
+
+
+
+
+
 
 __mod_name__ = " Games "
 
