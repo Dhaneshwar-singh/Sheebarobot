d9743243da905a6e0871caa0cfd90806f5179810
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-01 10:35:25 +0530

    Create __games.py

---
+++ SheebaQueen/modules/__games.py
@@ -0,0 +1,25 @@
+from SheebaQueen.events import register
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@register(pattern="^/game")
+
+async def game(event): 
+
+      await event.reply("/no : makin 2048 no.")
+#2048 game
+f_img="https://telegra.ph/file/dbe8d49976baf292bb372.png"
+
+@register(pattern="^/no")
+
+async def no(event): 
+
+      await event.reply(f_img, [
+    [
+        InlineKeyboardButton(
+            text="2048", url="sheebaga.heliohost.com"),
+    ],
+
+])
+                  
+
