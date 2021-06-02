52eef8bbf08007355a39f47e56f4f9d89529bef2
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-02 11:30:20 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -11,9 +11,9 @@ async def game(event):
       await event.reply("These Are Some Best Game Providided By Me To You",
 buttons = [[
 Button.url("2048 No. Game" , 
-url ="sheebaga.heliohost.us")]])
-#[[Button.url("Pokeball slice",
-#url="sheebaga.heliohost.us/Pokemonslice.html")
+url ="sheebaga.heliohost.us")]]
+[[Button.url("Pokeball slice",
+url="sheebaga.heliohost.us/Pokemonslice.html")
 #Button.url("Rome Run",url="https://games.cdn.famobi.com/html5games/o/om-nom-run/v1140/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abe80572-560a-444d-baf7-2fa4a7b2c02f&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=177&original_ref=android-app%3A%2F%2Forg.telegram.messenger%2F")
 
 
@@ -21,7 +21,7 @@ url ="sheebaga.heliohost.us")]])
 
 
 
-#]]  )                
+]]  )                
 
 __mod_name__ = "Game"
 __help__ ="""
