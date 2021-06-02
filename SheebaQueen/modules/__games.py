9332fc118716192375a01b06062ca1c00911b4de
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-02 11:53:18 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -1,29 +1,47 @@
 from SheebaQueen.events import register
-from telethon import Button
-
-
+from telethon import Buttons
 
 
 @register(pattern="^/game")
 
 async def game(event): 
 
-      await event.reply("These Are Some Best Game Providided By Me To You",
-buttons = [[
-Button.url("2048 No. Game" , 
-url ="sheebaga.heliohost.us")]],
-[[Button.url("Pokeball slice",
-url="sheebaga.heliohost.us/Pokemonslice.html")
-#Button.url("Rome Run",url="https://games.cdn.famobi.com/html5games/o/om-nom-run/v1140/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abe80572-560a-444d-baf7-2fa4a7b2c02f&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=177&original_ref=android-app%3A%2F%2Forg.telegram.messenger%2F")
+      await event.reply("/no : makin 2048 no.
+,/run : for run game
+, /slice : Pokemon slice game")
+#2048 game
+
+@register(pattern="^/no")
+
+async def no(event): 
+
+      await event.reply("Some Games By Me"
+,button=Button.url([["2048 game ",url="sheebaga.heliohost.us")]])
+#pokemon sluce
+
+@register(pattern="^/slice")
+
+async def slice(event): 
+
+      await event.reply("Some Games By Me"
+,button=[[Button.url("2048 game ",url="sheebaga.heliohost.us/pokemonslice.html")]])
+
+#Rom run
+
+@register(pattern="^/run")
+
+async def run(event): 
+
+      await event.reply("Some Games By Me"
+,button=[[Button.url("2048 game ",url="https://games.cdn.famobi.com/html5games/o/om-nom-run/v1140/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abe80572-560a-444d-baf7-2fa4a7b2c02f&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=177&original_ref=android-app%3A%2F%2Forg.telegram.messenger%2F")]])
 
 
-
-
-
-
-]]  )                
 
 __mod_name__ = "Game"
 __help__ ="""
  /game ==>> get button for all available games
 """
+
+
+
+
