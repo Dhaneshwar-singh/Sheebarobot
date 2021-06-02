783924e0772d35bc9285918aa1ca4a33f61018b6
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-02 12:12:44 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -6,7 +6,9 @@ from telethon import Button
 
 async def game(event): 
 
-      await event.reply("/no : makin 2048 no. /run : run game /slice : Pokeball slice game")
+      await event.reply("/no : makin 2048 no.
+ /run : run game 
+/slice :  ball slice game")
 #2048 game
 
 @register(pattern="^/no")
@@ -22,7 +24,7 @@ async def no(event):
 async def slice(event): 
 
       await event.reply("Some Games By Me"
-,button=[[Button.url("2048 game ",url="sheebaga.heliohost.us/pokemonslice.html")]])
+,buttons=[[Button.url("Pokeball slice ",url="sheebaga.heliohost.us/pokemonslice.html")]])
 
 #Rom run
 
@@ -31,7 +33,7 @@ async def slice(event):
 async def run(event): 
 
       await event.reply("Some Games By Me"
-,button=[[Button.url("2048 game ",url="https://games.cdn.famobi.com/html5games/o/om-nom-run/v1140/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abe80572-560a-444d-baf7-2fa4a7b2c02f&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=177&original_ref=android-app%3A%2F%2Forg.telegram.messenger%2F")]])
+,buttons=[[Button.url("Rom Run ",url="https://games.cdn.famobi.com/html5games/o/om-nom-run/v1140/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=abe80572-560a-444d-baf7-2fa4a7b2c02f&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=177&original_ref=android-app%3A%2F%2Forg.telegram.messenger%2F")]])
 
 
 
