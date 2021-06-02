fed043bb7169d704984fde6c5ee470b867d5b17c
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-02 23:38:32 +0530

    Update logomaker.py

--- SheebaQueen/modules/logomaker.py
+++ SheebaQueen/modules/logomaker.py
@@ -20,7 +20,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open(random.choice('./SheebaQueen/resources/{}.jpg'))
+    img = Image.open(random.choice('./SheebaQueen/resources/*'))
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
