95191a834ba57298b030ae615432c409ebab81a3
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-03 12:22:10 +0530

    Update logomaker.py

--- SheebaQueen/modules/logomaker.py
+++ SheebaQueen/modules/logomaker.py
@@ -4,7 +4,7 @@ from SheebaQueen import telethn as tbot
 import os 
 from PIL import Image, ImageDraw, ImageFont
 import random
-im = [./SheebaQueen/img/"download (14).jpeg","download (15).jpeg"]
+im = (/SheebaQueen/img/"download (14).jpeg","download (15).jpeg")
 @register(pattern="^/logo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
