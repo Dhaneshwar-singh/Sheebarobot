a8b3c3fd9ea996618eb427e379463a4421ffda21
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-04 20:21:26 +0530

    Update mmf.py

--- SheebaQueen/modules/mmf.py
+++ SheebaQueen/modules/mmf.py
@@ -43,7 +43,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./SheebaQueen/resources/Russo.ttf"
+        fnt = "./SheebaQueen/resources/Maghrib.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
