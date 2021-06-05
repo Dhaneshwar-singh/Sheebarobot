56d2401d70a4753da9c8b67bca8df75955d1187d
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-05 08:27:35 +0530

    Update logo.py

--- SheebaQueen/modules/logo.py
+++ SheebaQueen/modules/logo.py
@@ -1,114 +1,80 @@
 
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-"""
-✘ Commands Available -
-• `{i}logo <text>`
-   Generate a logo of the given Text
-   Or Reply To image , to write ur text on it.
-   Or Reply To Font File, To write with that font.
-"""
-
-import glob
 import os
 import random
-import time
 
-from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import InputMessagesFilterPhotos
+import requests
+from bs4 import *
+from pyrogram import filters
+
+from SheebaQueen.pyrogramee.pluginshelper import admins_only, get_text
+from SheebaQueen.resources.pyrogram import pbot
 
 
-from SheebaQueen.events import register
+def download_images(images):
+    count = 0
+    print(f"Total {len(images)} Image Found!")
+    if len(images) != 0:
+        for i, image in enumerate(images):
+            try:
+                image_link = image["data-srcset"]
+            except:
+                try:
+                    image_link = image["data-src"]
+                except:
+                    try:
+                        image_link = image["data-fallback-src"]
+                    except:
+                        try:
+                            image_link = image["src"]
+                        except:
 
-@register(pattern="^/logo")
-async def logo_gen(event):
-    xx = await (event, get_string("com_1"))
-    name = event.pattern_match.group(1)
-    if not name:
-        await eod(xx, "`Give a name too!`")
-    bg_, font_ = "https://source.unsplash.com/random", ""
-    if event.reply_to_msg_id:
-        temp = await event.get_reply_message()
-        if temp.media:
-            if hasattr(temp.media, "document"):
-                if "font" in temp.file.mime_type:
-                    font_ = await temp.download_media()
-                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
-                    font_ = await temp.download_media()
-            elif "pic" in mediainfo(temp.media):
-                bg_ = await temp.download_media()
-    else:
-        pics = []
-        async for i in ultroid.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-        fpath_ = "./resources/fonts/"
-        f = random.choice(os.listdir(fpath_))
-        font_ = fpath_ + f
-    if not bg_:
-        pics = []
-        async for i in ultroid.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-    if not font_:
-        fpath_ = glob.glob("resources/fonts/*")
-        font_ = random.choice(fpath_)
-    if len(name) <= 8:
-        fnt_size = 150
-        strke = 10
-    elif len(name) >= 9:
-        fnt_size = 50
-        strke = 5
-    else:
-        fnt_size = 130
-        strke = 20
-    img = Image.open(bg_)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, fnt_size)
-    w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
-    image_width, image_height = img.size
-    draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
-        name,
-        font=font,
-        fill=(255, 255, 255),
-    )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text(
-        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
-    )
-    flnme = f"ultd.png"
-    img.save(flnme, "png")
-    await xx.edit("`Done!`")
-    if os.path.exists(flnme):
-        tt = time.time()
-        up = await uploader(flnme, flnme, tt, xx, "Uploading...")
-        await ultroid.send_file(
-            event.chat_id,
-            file=up,
-            caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
-            foce_document=True,
+                            pass
+            try:
+                r = requests.get(image_link).content
+                try:
+
+                    r = str(r, "utf-8")
+                except UnicodeDecodeError:
+                    with open("logo@AsunaBOT.jpg", "wb+") as f:
+                        f.write(r)
+                    count += 1
+            except:
+                pass
+
+
+def mainne(name, typeo):
+    url = f"https://source.unsplash.com/random={name}&searchtext={typeo}&searchService="
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    images = soup.findAll("img")
+    random.shuffle(images)
+    if images is not None:
+        print("level 1 pass")
+    download_images(images)
+
+
+@pbot.on_message(filters.command("logo") & ~filters.edited & ~filters.bot)
+@admins_only
+async def logogen(client, message):
+    pablo = await client.send_message(message.chat.id, "`Creating The Logo.....`")
+    Godzilla = get_text(message)
+    if not Godzilla:
+        await pablo.edit("Invalid Command Syntax, Please Check Help Menu To Know More!")
+        return
+    lmao = Godzilla.split(":", 1)
+    try:
+        typeo = lmao[1]
+    except BaseException:
+        typeo = "name"
+        await pablo.edit(
+            "Give name and type for logo Idiot. like `/logogen Made By Asuna_Robot`"
         )
-        os.remove(flnme)
-        await xx.delete()
-    if os.path.exists(bg_):
-        os.remove(bg_)
-    if os.path.exists(font_):
-        if not font_.startswith("resources/fonts"):
-            os.remove(font_)
-
-
-
+    name = lmao[0]
+    mainne(name, typeo)
+    pate = "logo@AsunaBOT.jpg"
+    await client.send_photo(message.chat.id, pate)
+    try:
+        os.remove(pate)
+    except:
+        pass
+    await pablo.delete()
