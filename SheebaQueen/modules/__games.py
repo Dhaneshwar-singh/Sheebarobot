b7fe7164136b86c8a37547d815d2c1e987992423
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-05 08:55:39 +0530

    Update __games.py

--- SheebaQueen/modules/__games.py
+++ SheebaQueen/modules/__games.py
@@ -8,7 +8,7 @@ from telethon import Button
 
     # await event.reply("/no : makin 2048 no. /run : run game  /slice :  ball slice game")
 #2048 game
-
+im = ("./SheebaQueen/img/g.png")
 @register(pattern="^/no")
 
 async def no(event): 
