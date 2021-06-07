c3cf5a4ac561173965534cd0bd2a2c491dd704e1
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-07 13:37:09 +0530

    Create queues.py

---
+++ SheebaQueen/handlers/queues.py
@@ -0,0 +1,23 @@
+def func(func_, *args, **kwargs) -> dict:
+
+    return {"func": func_, "args": [*args], "kwargs": dict(**kwargs)}
+
+def run(_: dict, **kwargs):
+
+    return _["func"](
+
+        *_["args"],
+
+        **(
+
+            {
+
+                **_["kwargs"],
+
+                **(dict(**kwargs))
+
+            }
+
+        )
+
+    )
